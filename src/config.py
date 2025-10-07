e25ef45c6094795651aa821a6b264f2ff4a302ca
Akhila Dubasi <akhiladubsi45@gmail.com>
2025-10-08 00:53:20 +0530

    update

--- src/config.py
+++ src/config.py
@@ -1,10 +1,7 @@
-from supabase import create_client
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
+from supabase import create_client, Client
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
-supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
